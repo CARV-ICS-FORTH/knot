b6348f38d1e0c79bf903db2432744c3a4f613f36
Antony Chazapis <chazapis@gmail.com>
2020-03-31 12:39:04 +0300

    API documentation

--- client/karvdash_client/api.py
+++ client/karvdash_client/api.py
@@ -22,6 +22,20 @@ except ImportError:
 
 
 class API:
+    """Python interface to the Karvdash API.
+
+    :param config_path: file containing configuration options for the API endpoint
+    :type config_path: string
+
+    The provided config file should have the following structure (example values provided)::
+
+        [Karvdash]
+        base_url = http://127.0.0.1:8000/api
+        token = 8e41faf30b7bee20725a49bd73bba680055b95c1
+
+    If no config file is provided, the following paths are tried in order: ``~/.karvdash/config.ini``, ``/var/lib/karvdash/config.ini``.
+    """
+
     def __init__(self, config_path=None):
         self.config_path = config_path if config_path else self._get_config_path()
         self.base_url, self.token = self._get_configuration(self.config_path)
@@ -52,28 +66,107 @@ class API:
         return {'Authorization': 'Token %s' % self.token.lower()}
 
     def list_services(self):
+        """List running services.
+
+        :returns: A list of services
+
+        Each service is represented with a dictionary containing the following keys:
+
+        ============  ================  ================================================
+        Key           Type              Description
+        ------------  ----------------  ------------------------------------------------
+        ``name``      <string>          The service name
+        ``url``       <string>          The URL to access the service frontend
+        ``created``   <string>          When the service was created (not included
+
+                                        in response to service create calls)
+        ``actions``   <boolean>         ``True`` if service can be deleted
+        ``template``  <dictionary>      Service template information
+        ============  ================  ================================================
+
+        For a list of keys in the template dictionary, consult
+        the ``list_services()`` function documentation.
+        """
+
         r = requests.get(self.base_url + '/services/', headers=self._headers)
         return r.json()
 
     def create_service(self, filename, variables):
+        """Create/start a service.
+
+        :param filename: the template filename
+        :param variables: template variables as key-value pairs (provide at least a ``name`` key)
+        :type filename: string
+        :type variables: dictionary
+        :returns: The service created
+        """
+
         data = {k.upper(): v for k, v in variables.items()}
         data['filename'] = filename
         r = requests.post(self.base_url + '/services/', json=data, headers=self._headers)
         return r.json()
 
     def exec_service(self, name, command):
+        """Execute a command at service pods.
+
+        :param command: the command to execute
+        :type command: string
+        :returns: A list of results
+        """
+
         data = {'command': command}
         r = requests.post(self.base_url + '/services/%s/' % name, json=data, headers=self._headers)
         return r.json()
 
     def delete_service(self, name):
+        """Delete/stop a running service.
+
+        :param name: the service to delete
+        :type name: string
+        :returns: ``True`` if the call was successful
+        """
+
         r = requests.delete(self.base_url + '/services/%s/' % name, headers=self._headers)
         return r.status_code == requests.codes.no_content
 
     def list_templates(self):
+        """List available templates.
+
+        :returns: A list of templates
+
+        return {'name': self._name,
+                'description': self._description,
+                'singleton': self._singleton,
+                'mount': self._mount,
+                'variables': self._variables}
+
+        Each template is represented with a dictionary containing the following keys:
+
+        ===============  ================  =========================================================
+        Key              Type              Description
+        ---------------  ----------------  ---------------------------------------------------------
+        ``filename``     <string>          The template filename
+        ``name``         <string>          The template name as shown in the dashboard
+        ``description``  <string>          The template description as shown in the dashboard
+        ``singleton``    <boolean>         ``True`` if only one instance can be running
+        ``mount``        <boolean>         ``True`` if instances have all mountpoints attached
+        ``variables``    <dictionary>      Template variables
+        ``values``       <dictionary>      Instance values for template variables (included
+
+                                           when template is returned as part of a service)
+        ===============  ================  =========================================================
+        """
+
         r = requests.get(self.base_url + '/templates/', headers=self._headers)
         return r.json()
 
     def inject(self, data):
+        """Inject storage paths to a service description.
+
+        :param data: the service description (in YAML format)
+        :type data: string
+        :returns: The updated service description (in YAML format)
+        """
+
         r = requests.post(self.base_url + '/utils/inject/', data=data, headers=self._headers)
         return r.text
