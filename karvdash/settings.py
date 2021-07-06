7ccc1b5f363f02733dca29dfc9df0241d1d3c06f
Antony Chazapis <chazapis@ics.forth.gr>
2021-07-06 15:17:40 +0300

    Update requirements

--- karvdash/settings.py
+++ karvdash/settings.py
@@ -126,6 +126,8 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
