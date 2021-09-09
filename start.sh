2c3cb8085f10268a132c9b48a7ed34fc2eba65d2
Antony Chazapis <chazapis@ics.forth.gr>
2021-09-09 22:49:40 +0300

    Configure Vouch secret in the proper namespace (#26)

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ ADMIN_PASSWORD=${KARVDASH_ADMIN_PASSWORD:-admin}
 
 python manage.py migrate
 python manage.py createadmin --noinput --username admin --password $ADMIN_PASSWORD --email admin@example.com --preserve
-python manage.py createoauthapplication --name vouch --redirect-uri $KARVDASH_VOUCH_URL/auth --secret-name $KARVDASH_VOUCH_SECRET
+python manage.py createoauthapplication --name vouch --redirect-uri $KARVDASH_VOUCH_URL/auth --secret-name $KARVDASH_VOUCH_SECRET --secret-namespace $KARVDASH_NAMESPACE
 if [[ -n $KARVDASH_JUPYTERHUB_URL && -n $KARVDASH_JUPYTERHUB_NAMESPACE ]]; then
     python manage.py createoauthapplication --name jupyterhub --redirect-uri $KARVDASH_JUPYTERHUB_URL/hub/oauth_callback --secret-name karvdash-oauth-jupyterhub --secret-namespace $KARVDASH_JUPYTERHUB_NAMESPACE
 fi
