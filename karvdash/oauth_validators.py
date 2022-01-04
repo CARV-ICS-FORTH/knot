3e5e8750132dff4d429839fc5e31d04d1f91bb26
Antony Chazapis <chazapis@ics.forth.gr>
2022-01-04 12:50:10 +0200

    Return environment with OIDC

--- karvdash/oauth_validators.py
+++ karvdash/oauth_validators.py
@@ -12,16 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from django.conf import settings
+from urllib.parse import urlparse
 from oauth2_provider.oauth2_validators import OAuth2Validator
 
 
 class CustomOAuth2Validator(OAuth2Validator):
     def get_additional_claims(self, request):
+        registry_url = urlparse(settings.REGISTRY_URL)
         return {
-            "sub": request.user.username,
-            "preferred_username": request.user.username,
-            "email": request.user.email,
-            "name": request.user.get_full_name(),
-            "given_name": request.user.first_name,
-            "family_name": request.user.last_name,
+            'sub': request.user.username,
+            'preferred_username': request.user.username,
+            'email': request.user.email,
+            'name': request.user.get_full_name(),
+            'given_name': request.user.first_name,
+            'family_name': request.user.last_name,
+            'karvdash_namespace': 'karvdash-%s' % request.user.username,
+            'karvdash_ingress_url': settings.INGRESS_URL,
+            'karvdash_registry_url': '%s://%s:%s' % (registry_url.scheme, registry_url.hostname, registry_url.port),
+            'karvdash_argo_workflows_url': settings.ARGO_WORKFLOWS_URL
         }
